4f01fb9f6e2e8e4c922d36e7a5938b0a4302a962
WAWAYU-AI <1942896934@qq.com>
2026-02-11 21:09:04 +0800

    完善

--- install.sh
+++ install.sh
@@ -13,8 +13,24 @@ link_file () {
   fi
 
   ln -s "$src" "$dst"
-  echo "Linked $dst"
+  echo "Linked $dst -> $src"
 }
 
+echo "Installing dotfiles from $DOTFILES_DIR"
+echo "---------------------------------------"
+
+# Zsh
 link_file "$DOTFILES_DIR/zsh/.zshrc" "$HOME/.zshrc"
+
+# Bash
+link_file "$DOTFILES_DIR/bash/.bashrc" "$HOME/.bashrc"
+
+# Git
+link_file "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
+
+# Tmux
 link_file "$DOTFILES_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+
+echo "---------------------------------------"
+echo "Dotfiles installation complete ✅"
+
